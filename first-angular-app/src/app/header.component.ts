80173cbd267ff191efc4bba081103b751b8d3d1e
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2025-02-26 07:57:36 +0530

    16

--- first-angular-app/src/app/header.component.ts
+++ first-angular-app/src/app/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector : 'app-header',
-    template: './header.component.html',
+    templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
 
