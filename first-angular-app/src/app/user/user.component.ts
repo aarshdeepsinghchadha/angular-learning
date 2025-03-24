29721a1d4c455053be7e158afe260d2c8d7b3463
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2025-03-24 23:20:36 +0530

    module3

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,22 +1,31 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// }
+
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
+
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter<string>();
+  @Input({required: true}) selected!: boolean;
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectedUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
